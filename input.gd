3f95ab80b38102876d9fcb58c19c16f517b4d350
ndikabunder <ndikabunder@gmail.com>
2025-10-14 15:44:07 +0700

    Menambahkan gitignore

--- input.gd
+++ input.gd
@@ -3,7 +3,11 @@ extends Node
 
 func _input(event):
 	if event.is_action_pressed("my_action"):
+		if has_node("Label"):
+			$Label.text = "Action Pressed!"
 		$Label.text = "Action Pressed!"
-
+	if event.is_action_released("my_action"):
+		if has_node("Label"):
+			$Label.text = "Action Released!"
 	if event.is_action_released("my_action"):
 		$Label.text = "Action Released!"
