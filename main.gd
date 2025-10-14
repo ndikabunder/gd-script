5570c523663e579c5dd146279ab2d68913bc3873
ndikabunder <ndikabunder@gmail.com>
2025-10-14 12:31:39 +0700

    Modifikasi node

--- main.gd
+++ main.gd
@@ -3,7 +3,4 @@ extends Node
 
 func _ready():
 	print("Hello, Godot!")
-
-
-func _process(delta):
-	pass  # Replace with function body.
+# Replace with function body.
---
+++ modifying_node.gd
@@ -0,0 +1,6 @@
+extends Node
+
+
+func _ready():
+	$Label.text = "Hello, Godot!"
+	$Label.modulate = Color.GREEN
